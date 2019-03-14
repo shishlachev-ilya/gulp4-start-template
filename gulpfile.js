26431566a2e1de082c528c2d5e285bfeb81fa6e4
ilya <shishlachevilya@gmail.com>
2019-03-14 17:19:45 +0200

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+let gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	csso = require('gulp-csso'),
+	notify = require('gulp-notify'),
+	babel = require("gulp-babel"),
+	uglify = require('gulp-uglify'),
+	sourcemaps = require('gulp-sourcemaps'),
+	autoprefixer = require('gulp-autoprefixer'),
+	htmlmin = require('gulp-html-minifier'),
+	browserSync = require('browser-sync').create();
+
+gulp.task('minify', function () {
+	return gulp.src('src/pages/*.html')
+	.pipe(htmlmin({collapseWhitespace: true}))
+	.pipe(gulp.dest('build'))
+});
+
+gulp.task('sass', function () {
+	return gulp.src('src/assets/scss/main.scss')
+	.pipe(sourcemaps.init())
+	.pipe(sass())
+	.on("error", notify.onError({
+		message: "Error: <%= error.message %>",
+		title: "Error running something"
+	}))
+	.pipe(autoprefixer(['last 10 versions', '> 1%', 'ie 8', 'ie 7'], {cascade: true}))
+	.pipe(csso())
+	.pipe(sourcemaps.write())
+	.pipe(gulp.dest('build/css'))
+});
+
+gulp.task("script", function () {
+	return gulp.src("src/assets/script/main.js")
+	.pipe(babel())
+	.pipe(uglify())
+	.pipe(gulp.dest("build/script"));
+});
+
+gulp.task('serve', function() {
+	browserSync.init({
+		server: {
+			baseDir: "./build"
+		}
+	});
+	browserSync.watch('build', browserSync.reload)
+});
+
+gulp.task('watch', function () {
+	gulp.watch('src/pages/*.html', gulp.series('minify'));
+	gulp.watch('src/assets/scss/main.scss', gulp.series('sass'));
+	gulp.watch('src/assets/script/*.js', gulp.series('script'));
+});
+
+gulp.task('default', gulp.series(
+	gulp.parallel('sass', 'minify', 'script'),
+	gulp.parallel('watch', 'serve')
+));
