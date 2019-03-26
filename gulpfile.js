f5f6a0b562b574f310285dd18aa72e31ea81d963
ilya <shishlachevilya@gmail.com>
2019-03-26 13:15:34 +0200

    add images compress task

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,23 @@ let gulp = require('gulp'),
 	autoprefixer = require('gulp-autoprefixer'),
 	htmlmin = require('gulp-html-minifier'),
 	sassLint = require('gulp-sass-lint'),
+	tinypng = require('gulp-tinypng'),
 	browserSync = require('browser-sync').create();
+
+
+gulp.task('img', function (done) {
+	gulp.src('src/assets/img/*.{png,jpg,jpeg}')
+	.pipe(gulp.dest('build/img'));
+	done();
+});
+
+gulp.task('tinypng', function (done) {
+	gulp.src('build/img/*.{png,jpg,jpeg}')
+	.pipe(tinypng('mt3FPFKp1LJMYTXrSMRJfm9tPKSygN5q'))
+	.pipe(gulp.dest('build/img'));
+	done();
+});
+
 
 gulp.task('lint', function () {
 	return gulp.src('src/assets/scss/**/*.scss')
@@ -57,9 +73,10 @@ gulp.task('watch', function () {
 	gulp.watch('src/pages/*.html', gulp.series('minify'));
 	gulp.watch('src/assets/scss/**/*.scss', gulp.series('sass'));
 	gulp.watch('src/assets/script/*.js', gulp.series('script'));
+	gulp.watch('src/assets/img/*.{png,jpg,jpeg}', gulp.series('img'));
 });
 
 gulp.task('default', gulp.series(
-	gulp.parallel('sass', 'minify', 'script'),
+	gulp.parallel('sass', 'minify', 'script', 'img'),
 	gulp.parallel('watch', 'serve')
 ));
